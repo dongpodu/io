305610e4343d4f095eeecffd5ef77d8b9ec990d2
willdu <willdu@vipabc.com>
2018-09-25 19:28:48 +0800

    filechannel test

--- src/com/elisonwell/nio/FileChannelTest.java
+++ src/com/elisonwell/nio/FileChannelTest.java
@@ -1,13 +1,8 @@
 package com.elisonwell.nio;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-
-import com.elisonwell.io.FileKit;
 
 /**
  * http://www.ibm.com/developerworks/cn/education/java/j-nio/
@@ -15,42 +10,56 @@ import com.elisonwell.io.FileKit;
  * @createAt 2016年6月3日
  */
 public class FileChannelTest {
-	
-	public static void save(FileInputStream in,FileOutputStream out){
-		FileChannel inChannel = in.getChannel();
-		FileChannel outChannel = out.getChannel();
+
+	/**
+	 * 将数据从channel读到buffer
+	 * @throws Exception
+	 */
+	public static void testRead() throws Exception {
+		String path = "D:\\work\\test.jpg";
+		/*
+		 * 不能用fileOutputStream，因为用fileOutputStream获取的channel调用read方法时，会报NonReadableChannelException
+		 */
+		FileInputStream in = new FileInputStream(new File(path));
+		FileChannel channel = in.getChannel();
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
-		try {
-			int i = 0;
-			while(true){
-				buffer.clear();
-				i = inChannel.read(buffer);
-				if(i==-1){
-					break;
-				}
-				buffer.flip();
-				outChannel.write(buffer);
+
+		while(true){
+			int count = channel.read(buffer);
+			System.out.println("字节数："+count+"，位置："+buffer.position());
+			if(count==-1){
+				break;
 			}
-			in.close();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+			/*
+			buffer内部维护了position属性，每次将数据读到buffer时，都是将数据读到buffer内以position开始依次向后的位置，且使得position后移，
+			所以如果没有buffer.flip()，那么在第一次读完后，就无法再读取数据了。
+			 */
+			buffer.flip();
 		}
 	}
-	
+
+	/**
+	 * 将数据从buffer写到channel
+	 * @throws Exception
+	 */
+	public static void testWrite() throws Exception {
+		String test = "New String to write to file";
+		/*
+		 * 不能用fileInputStream，因为用fileInputStream获取的channel调用write方法时，会报NonWritableChannelException
+		 */
+		FileOutputStream out = new FileOutputStream(new File("D:\\work\\test.txt"));
+		FileChannel outChannel = out.getChannel();
+		ByteBuffer buffer = ByteBuffer.allocate(84);
+		buffer.clear();
+		buffer.put(test.getBytes());
+		buffer.flip();
+		while (buffer.hasRemaining()) {
+			outChannel.write(buffer);
+		}
+	}
+
 
 	public static void main(String[] args) throws Exception{
-		File src = new File("/Users/duyisong/Documents/life/video/yiren.mp4");
-		FileInputStream in = new FileInputStream(src);
-		FileOutputStream out = new FileOutputStream(
-				new File("/Users/duyisong/Downloads/nio/yiren.mp4"));
-		long t = System.currentTimeMillis();
-		FileKit.saveFile(src, "mp4");
-		long t1 = System.currentTimeMillis();
-		System.out.println(t1-t);
-		
-//		save(in, out);
-//		long t2 = System.currentTimeMillis();
-//		System.out.println(t2-t1);
+		testRead();
 	}
 }
