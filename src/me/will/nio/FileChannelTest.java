497cd189a01626fc65ccbb2562be286928ae3dc0
duyisong <duyisong@bilibili.com>
2021-04-25 19:01:09 +0800

    test

--- src/me/will/nio/FileChannelTest.java
+++ src/me/will/nio/FileChannelTest.java
@@ -1,65 +1,88 @@
 package me.will.nio;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
  * http://www.ibm.com/developerworks/cn/education/java/j-nio/
+ *
  * @author duyisong
  * @createAt 2016年6月3日
  */
 public class FileChannelTest {
 
-	/**
-	 * 将数据从channel读到buffer
-	 * @throws Exception
-	 */
-	public static void testRead() throws Exception {
-		String path = "/Users/duyisong/Downloads/download.pdf";
-		/*
-		 * 不能用fileOutputStream，因为用fileOutputStream获取的channel调用read方法时，会报NonReadableChannelException
-		 */
-		FileInputStream in = new FileInputStream(new File(path));
-		FileChannel channel = in.getChannel();
-		ByteBuffer buffer = ByteBuffer.allocate(1024);
+    /**
+     * 将数据从channel读到buffer
+     *
+     * @throws Exception
+     */
+    public static void testRead() throws Exception {
+        String path = "/Users/duyisong/Downloads/download.pdf";
+        /*
+         * 不能用fileOutputStream，因为用fileOutputStream获取的channel调用read方法时，会报NonReadableChannelException
+         */
+        FileInputStream in = new FileInputStream(new File(path));
+        FileChannel channel = in.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-		while(true){
-			int count = channel.read(buffer);
-			System.out.println("字节数："+count+"，位置："+buffer.position());
-			if(count==-1){
-				break;
-			}
-			/*
-			buffer内部维护了position属性，每次将数据读到buffer时，都是将数据读到buffer内以position开始依次向后的位置，且使得position后移，
-			所以如果没有buffer.flip()，那么在第一次读完后，就无法再读取数据了。
-			 */
-			buffer.flip();
-		}
-	}
+        while (true) {
+            int count = channel.read(buffer);
+            System.out.println("字节数：" + count + "，位置：" + buffer.position());
+            if (count == -1) {
+                break;
+            }
+            buffer.clear();
+        }
+    }
 
-	/**
-	 * 将数据从buffer写到channel
-	 * @throws Exception
-	 */
-	public static void testWrite() throws Exception {
-		String test = "New String to write to file";
-		/*
-		 * 不能用fileInputStream，因为用fileInputStream获取的channel调用write方法时，会报NonWritableChannelException
-		 */
-		FileOutputStream out = new FileOutputStream(new File("D:\\work\\test.txt"));
-		FileChannel outChannel = out.getChannel();
-		ByteBuffer buffer = ByteBuffer.allocate(84);
-		buffer.clear();
-		buffer.put(test.getBytes());
-		buffer.flip();
-		while (buffer.hasRemaining()) {
-			outChannel.write(buffer);
-		}
-	}
+    /**
+     * 复制文件
+     *
+     * @throws Exception
+     */
+    public static void testTransfer() throws Exception {
+        FileInputStream in = new FileInputStream(new File("/Users/bilibili/Downloads/download.pdf"));
+        FileChannel channel = in.getChannel();
+
+        FileOutputStream out = new FileOutputStream(new File("/Users/bilibili/Downloads/download1.pdf"));
+
+        long bytes = 10000;
+        long position = 0;
+        while (true) {
+            long realTransferBytes = channel.transferTo(position, bytes, out.getChannel());
+            if (realTransferBytes == 0) {
+                break;
+            }
+            position += realTransferBytes;
+        }
+    }
+
+    /**
+     * 将数据从buffer写到channel
+     *
+     * @throws Exception
+     */
+    public static void testWrite() throws Exception {
+        String test = "New String to write to file";
+        /*
+         * 不能用fileInputStream，因为用fileInputStream获取的channel调用write方法时，会报NonWritableChannelException
+         */
+        FileOutputStream out = new FileOutputStream(new File("D:\\work\\test.txt"));
+        FileChannel outChannel = out.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(84);
+        buffer.clear();
+        buffer.put(test.getBytes());
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            outChannel.write(buffer);
+        }
+    }
 
 
-	public static void main(String[] args) throws Exception{
-		testRead();
-	}
+    public static void main(String[] args) throws Exception {
+        testTransfer();
+    }
 }
--- src/me/will/nio/SelectorTest.java
+++ src/me/will/nio/SelectorTest.java
@@ -1,9 +1,66 @@
 package me.will.nio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 1、一个selector可以处理多个通道
+ * 2、与Selector一起使用时，Channel必须处于非阻塞模式下
+ * 3、有四种事件类型：Connect、Accept、Read、Write，不同的channel支持的事件类型可能不同，但都在这四个事件类型之内
+ * 4、当一个channel对多个事件感兴趣时，可以在注册时传递多个事件的或运算结果，如SelectionKey.OP_ACCEPT | SelectionKey.OP_CONNECT
+ * 5、判断事件是否就绪可以使用：A、selectionKey.readyOps();  B、selectionKey.isAcceptable()...  C、selector.select()
+ * 6、
+ */
 public class SelectorTest {
 
-    public void test(){
+    public static void test() throws IOException {
+        Selector selector = Selector.open();
 
+        ServerSocketChannel channel8081 = ServerSocketChannel.open();
+        channel8081.bind(new InetSocketAddress(8001))
+                .configureBlocking(false);
+        SelectionKey key8001 = channel8081.register(selector, SelectionKey.OP_ACCEPT); //ServerSocketChannel只支持accept事件
+
+        ServerSocketChannel channel8082 = ServerSocketChannel.open();
+        channel8082.bind(new InetSocketAddress(8002))
+                .configureBlocking(false);
+        SelectionKey key8002 = channel8082.register(selector, SelectionKey.OP_ACCEPT);
+
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        while (true) {
+            System.out.println("-----检测通道是否就绪-----");
+            int count = selector.select();
+            System.out.println(String.format("-----%s个通道就绪-----", count));
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                System.out.println(String.format("%s就绪", serverSocketChannel));
+
+                SocketChannel socketChannel = serverSocketChannel.accept();
+
+                int readCount = socketChannel.read(buffer);
+                buffer.flip();
+                byte[] bytes = new byte[buffer.limit()];
+                buffer.get(bytes);
+
+                System.out.println(String.format("读取%s字节", readCount));
+                System.out.println(String.format("读取的内容：%s", new String(bytes)));
+                keyIterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        test();
     }
 
 }
